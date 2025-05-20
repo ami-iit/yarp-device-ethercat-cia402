615bd771426c27039c492c54f6f259b4da84c67b
Giulio Romualdi <giulio.romualdi@gmail.com>
2025-05-20 12:36:23 +0200

    Implement the Ds402MotionControl::open  (#2)

--- Ds402MotionControl/Ds402MotionControl.h
+++ Ds402MotionControl/Ds402MotionControl.h
@@ -4,10 +4,10 @@
 #ifndef YARP_DEV_DS402_MOTION_CONTROL_H
 #define YARP_DEV_DS402_MOTION_CONTROL_H
 
+#include <memory>
 #include <string>
 
 #include <yarp/dev/DeviceDriver.h>
-#include <yarp/os/Log.h>
 #include <yarp/os/PeriodicThread.h>
 
 namespace yarp
@@ -23,19 +23,64 @@ namespace dev
 class Ds402MotionControl : public DeviceDriver, public yarp::os::PeriodicThread
 {
 public:
+    /**
+     * @brief Constructor.
+     *
+     * @param period The period of the thread in seconds.
+     * @param useSystemClock Whether to use the system clock for timing.
+     */
     explicit Ds402MotionControl(double period,
                                 yarp::os::ShouldUseSystemClock useSystemClock
-                                = yarp::os::ShouldUseSystemClock::No);
+                                = yarp::os::ShouldUseSystemClock::Yes);
+    /**
+     * @brief Default constructor.
+     *
+     * This constructor sets the period to 0.01 seconds and uses the system clock.
+     */
     Ds402MotionControl();
 
-    ~Ds402MotionControl() override = default;
+    /**
+     * @brief Destructor.
+     *
+     * Cleans up the resources used by the driver.
+     */
+    ~Ds402MotionControl() override;
 
-    // ---------------- DeviceDriver ----------------
+    // clang-format off
+    /**
+     * @brief Opens the device driver.
+     *
+     * @param config The configuration parameters for the driver.
+     * @note The configuration parameters should include:
+     * | Parameter Name         | Type     | Description                                   | Is Mandatory? |
+     * |:----------------------:|:--------:|-----------------------------------------------|:-------------:|
+     * | ifname                 | string   | Name of the network interface to use          | Yes           |
+     * | num_axes               | int      | Number of axes to control                     | Yes           |
+     * | first_slave            | int      | Index of the slave to start from (default: 1) | No            |
+     * | expected_slave_name    | string   | Expected name of the slave                    | No            |
+     * @return true if the driver was opened successfully, false otherwise.
+     */
     bool open(yarp::os::Searchable& config) override;
+    // clang-format on
+
+    /**
+     * @brief Closes the device driver.
+     *
+     * @return true if the driver was closed successfully, false otherwise.
+     */
     bool close() override;
 
     // ---------------- PeriodicThread --------------
+    /**
+     * @brief Runs the periodic thread.
+     *
+     * This function is called periodically at the specified interval.
+     */
     void run() override;
+
+private:
+    struct Impl;
+    std::unique_ptr<Impl> m_impl;
 };
 
 } // namespace dev
